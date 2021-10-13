2969a606ce954ff253b135963fc16a985402ae55
adiporn <adiporn@hot-now.com>
2021-10-13 12:57:34 +0700

    addEnzyme testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,29 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import { shallow } from 'enzyme';
+import PersonList from './PersonList'
+describe("App", () => {
+  let appWrapper
+  beforeAll(() => {
+    appWrapper = shallow(<App/>)
+  });
 
-it('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  
+
+  test('renders a person list',() => {
+    const personList = appWrapper.find(PersonList)
+    
+    expect(personList).toHaveLength(1);
+  })
+  test('has state',() => {
+    const appState = appWrapper.state()
+
+    expect(appState).not.toBeNull();
+  })
+
+  it('',()=> {
+    const appState = appWrapper.state()
+
+    expect(appState.people).toBeDefined();
+  })
 });
